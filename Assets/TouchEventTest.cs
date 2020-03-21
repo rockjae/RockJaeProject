123280e3a748cb1ad543296e90c86e01cf04c04d
rockjae <gjy7448@gmail.com>
2020-03-22 05:36:32 +0900

    ㄱㄱ

---
+++ Assets/TouchEventTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchEventTest : MonoBehaviour
+{
+    private bool IsTouch = false;
+    public Transform testOBJ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void TouchEvent(bool IsTouch)
+    {
+        this.IsTouch = IsTouch;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsTouch)
+        {
+            testOBJ.localPosition += new Vector3(0, 0.1f*Time.deltaTime);
+        }
+    }
+}
