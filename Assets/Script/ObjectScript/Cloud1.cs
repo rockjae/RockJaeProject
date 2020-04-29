c9c60f97d7bba6b5a4524d38498a6ea475e50875
rockjae <my0075425@naver.com>
2020-04-29 18:44:47 +0900

    버그수정

--- Assets/Script/ObjectScript/Cloud1.cs
+++ Assets/Script/ObjectScript/Cloud1.cs
@@ -24,15 +24,21 @@ public class Cloud1 : MonoBehaviour
     private IEnumerator tileDown()
     {
         float time = 0;
+        player.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+
         while (true)
         {
             time += Time.deltaTime;
             this.transform.eulerAngles += new Vector3(0, 0, Time.deltaTime * 5000f);
             this.transform.position += new Vector3(0, Time.deltaTime*3f);
-            player.transform.position += new Vector3(0, Time.deltaTime * 1f);
+            
+            player.transform.position += new Vector3(0, Time.deltaTime * 3f);
             player.eulerAngles += new Vector3(0, 0,Time.deltaTime*5000f);
 
-            if (time > 3) { break; }
+            if (time > 3) {
+                player.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+                break;
+            }
             yield return null;
         }
         SceneController.Instance.ChangeScene(3);
---
+++ Assets/Script/ObjectScript/Monster2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster2 : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject == PlayerController.Instance.Player)
+        {
+            Debug.Log("Player Collision Monster2");
+            StartCoroutine(tileDown());
+        }
+    }
+    
+    private IEnumerator tileDown()
+    {
+        float time = 0;
+        while (true)
+        {
+            time += Time.deltaTime;
+            this.transform.position += new Vector3(0, -5f * Time.deltaTime);
+
+            if (time > 3) { break; }
+            yield return null;
+        }
+    }
+}
