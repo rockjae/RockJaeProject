f776d4b9a2bf5de13e5de7d297371d017b46af2f
rockjae <my0075425@naver.com>
2020-11-20 17:00:21 +0900

    타일 수정

--- Assets/Script/Tile/TileFishingShake.cs
+++ Assets/Script/Tile/TileFishingShake.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class TileFishingShake : MonoBehaviour
 {
+    private bool isDown = false;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject == PlayerController.Instance.Player)
+        if (!isDown && collision.gameObject == PlayerController.Instance.Player)
         {
             Debug.Log("Player Collision TileFishing");
             StartCoroutine(tileDown());
+            isDown = true;
         }
     }
 
@@ -19,6 +22,12 @@ public class TileFishingShake : MonoBehaviour
         yield return new WaitForSeconds(5f);
 
         this.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+        this.GetComponent<BoxCollider2D>().isTrigger = true;
         this.GetComponent<AudioSource>().Play();
+
+        this.GetComponent<Animator>().enabled = false;
+
+        yield return new WaitForSeconds(5f);
+        Destroy(transform.parent.gameObject);
     }
 }
